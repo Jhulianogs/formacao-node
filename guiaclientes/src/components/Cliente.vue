1234de013be898e85b76634c00ac50a5febaf1a0
Jhuliano <jhulianogs@gmail.com>
2021-12-28 10:37:50 -0300

    v1

---
+++ guiaclientes/src/components/Cliente.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Cliente: {{ nome }}</h2>
+        <h3>E-mail: {{ email }}</h3>
+        <hr>
+        <h4>One way</h4>
+        <input type="text" :value="nome"><br>
+        <input type="text" :value="descricao"><br>
+        <hr>
+        <h4>Two ways</h4>
+        <input type="text" v-model="nome"><br>
+        <input type="text" v-model="descricao"><br>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          nome: 'Juliano',
+          numero: '',
+          email: '', 
+          idade: 0,
+          descricao: 'Lorem ipsum'
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
