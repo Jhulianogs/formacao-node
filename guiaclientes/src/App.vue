15578f748fb4d13ffc33a3e76a796702a8c93c48
Jhuliano <jhulianogs@gmail.com>
2021-12-28 11:12:00 -0300

    v-if and etc

--- guiaclientes/src/App.vue
+++ guiaclientes/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <Cliente/>
+    <!-- <Cliente nome="Juliano"/>
+    <Cliente nome="Marta"/>
+    <Cliente nome="Pedro"/>
+    <Cliente :nome="nomeCliente"/> -->
+    <Cliente :cliente="clienteVitor" :showIdade="true" :showEmail="true" :isPremium="true"/>
+    <Cliente :cliente="clienteJuliano" :showIdade="true" :showEmail="false" :isPremium="false"/>
+    <Cliente :cliente="clienteMarta" :showIdade="false" :showEmail="true" :isPremium="false"/>
   </div>
 </template>
 
@@ -9,6 +15,26 @@
 import Cliente from './components/Cliente';
 export default {
   name: 'App',
+  data() {
+    return {
+        nomeCliente: 'Juliano Souza',
+        clienteVitor: {
+            nome: 'Vitor',
+            email: 'fdskafdsaklf@email.com.br',
+            idade: '22'
+        },
+        clienteJuliano: {
+            nome: 'Juliano',
+            email: 'jlifdasf@email.com.br',
+            idade: '35'
+        },
+        clienteMarta: {
+            nome: 'marta',
+            email: 'marta@email.com.br',
+            idade: '29'
+        }
+    }
+  },
   components: {
     Cliente,
   }
--- guiaclientes/src/components/Cliente.vue
+++ guiaclientes/src/components/Cliente.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        <h2>Cliente: {{ nome }}</h2>
-        <h3>E-mail: {{ email }}</h3>
+    <div :class="{'cliente': !isPremium, 'cliente-premium': isPremium}">
+        <!-- <h4>Nome: {{ nome}}</h4>
         <hr>
-        <h4>One way</h4>
-        <input type="text" :value="nome"><br>
-        <input type="text" :value="descricao"><br>
+        <p>{{ descricao}}</p>
         <hr>
-        <h4>Two ways</h4>
-        <input type="text" v-model="nome"><br>
-        <input type="text" v-model="descricao"><br>
+        <p>E-mail: {{ email}}</p> -->
+        <!-- ======================= -->
+        <h5>Cliente Objeto</h5>
+        <p>{{cliente.nome}}</p>
+        <p v-if="showIdade == true">Idade {{ cliente.idade }}</p>
+        <p v-else>Sem idade</p>
+        <p v-show="showEmail === true">E-mail {{cliente.email}}</p>
     </div>
 </template>
 
@@ -17,16 +18,38 @@
 export default {
   data() {
       return {
-          nome: 'Juliano',
-          numero: '',
-          email: '', 
-          idade: 0,
-          descricao: 'Lorem ipsum'
+        //   numero: '',
+        //   email: 'email@testes.com.br', 
+        //   descricao: 'Lorem ipsum',
+        // isPremium: false,
       }
+  },
+  props: {
+      nome: String,
+      cliente: Object,
+      idade: Number,
+      showIdade: Boolean,
+      showEmail: Boolean,
+      isPremium: Boolean
   }
 }
 </script>
 
 <style scoped>
+    .cliente {
+        background: rgb(245, 242, 242);
+        padding: 5px;
+        margin: 3px;
+        border: 1px solid rgba(0,0,0,0.3);
+        border-radius: 12px;
+    }
 
+    .cliente-premium {
+        background: rgb(32, 31, 31);
+        color: azure;
+        padding: 5px;
+        margin: 3px;
+        border: 1px solid rgba(0,0,0,0.3);
+        border-radius: 12px;
+    }
 </style>
