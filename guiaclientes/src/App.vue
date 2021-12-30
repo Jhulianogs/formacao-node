e6eb37b0613b91da856b5ae1a45e8bb27b89ca04
Jhuliano <jhulianogs@gmail.com>
2021-12-30 11:02:59 -0300

    VueJS

--- guiaclientes/src/App.vue
+++ guiaclientes/src/App.vue
@@ -1,23 +1,71 @@
 <template>
   <div id="app">
+    <div class="form">
+        <h3>Cadastro</h3>
+        <div class="error" v-show="error">
+            <p>Dados inválidos</p>
+        </div>
+        <input type="text" placeholder="Nome" v-model="nomeField"><br>
+        <input type="text" placeholder="email" v-model="emailField"><br>
+        <input type="number" placeholder="Idade" v-model="idadeField"><br>
+        <button @click="cadastrarUsuario()">Cadastrar</button>
+        <div class="buttons">
+          <button class="button is-primary">Primary</button>
+          <button class="button is-link">Link</button>
+        </div>
+    </div>
     <!-- <Cliente nome="Juliano"/>
     <Cliente nome="Marta"/>
     <Cliente nome="Pedro"/>
     <Cliente :nome="nomeCliente"/> -->
-    <Cliente :cliente="clienteVitor" :showIdade="true" :showEmail="true" :isPremium="true"/>
+    <!-- <Cliente :cliente="clienteVitor" :showIdade="true" :showEmail="true" :isPremium="true"/>
     <Cliente :cliente="clienteJuliano" :showIdade="true" :showEmail="false" :isPremium="false"/>
-    <Cliente :cliente="clienteMarta" :showIdade="false" :showEmail="true" :isPremium="false"/>
+    <Cliente :cliente="clienteMarta" :showIdade="false" :showEmail="true" :isPremium="false"/> -->
+    <div v-for="(cliente, index) in orderClientes" :key="cliente.id">
+        {{ index }}
+        <cliente :cliente="cliente"  @meDelete="deletarUsuario" :showEmail="true"/>
+        <hr>
+        <h4>Editar</h4>
+        <label for="">Nome</label>
+        <input type="text" v-model="cliente.nome">
+    </div>
+     
   </div>
 </template>
 
 <script>
-
+import _ from 'lodash';
 import Cliente from './components/Cliente';
 export default {
   name: 'App',
   data() {
     return {
+        error: false,
+        nomeField: '',
+        emailField: '',
+        idadeField: '',
+
         nomeCliente: 'Juliano Souza',
+        clientes: [
+            {
+                id: 1,
+                nome: 'Vitor',
+                email: 'fdskafdsaklf@email.com.br',
+                idade: '22'
+            },
+            {
+                id: 2,
+                nome: 'Juliano',
+                email: 'jlifdasf@email.com.br',
+                idade: '35'
+            },
+            {
+                id: 3,
+                nome: 'Marta',
+                email: 'marta@email.com.br',
+                idade: '29'
+            }
+        ],
         clienteVitor: {
             nome: 'Vitor',
             email: 'fdskafdsaklf@email.com.br',
@@ -36,7 +84,41 @@ export default {
     }
   },
   components: {
-    Cliente,
+    Cliente
+  },
+  methods: {
+      cadastrarUsuario: function() {
+          if (this.nomeField == ''|| this.emailField == '' || this.nomeField.length < 3) {
+            this.error = true;
+          } else {
+            this.clientes.push({
+                nome: this.nomeField,
+                email: this.emailField,
+                idade: this.idadeField,
+                id: Date.now()
+            });
+            this.nomeField = '';
+            this.emailField = '';
+            this.idadeField = '';
+            this.error = false;
+        }
+      }, 
+      deletarUsuario: function($event) {
+        console.log('receb');
+        console.log($event);
+        // $event.component.isPremium =true;
+        var id = $event.idCliente;
+        this.clientes = this.clientes.filter(cliente => cliente.id != id);
+        
+        // console.log(id);
+        // $event.component.testar();
+      }
+  },
+  computed: {
+    orderClientes: function() {
+      
+      return _.orderBy(this.clientes, ['nome'], ['asc'])
+    }
   }
 }
 </script>
@@ -50,4 +132,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
--- guiaclientes/src/components/Cliente.vue
+++ guiaclientes/src/components/Cliente.vue
@@ -1,55 +1,85 @@
 <template>
-    <div :class="{'cliente': !isPremium, 'cliente-premium': isPremium}">
-        <!-- <h4>Nome: {{ nome}}</h4>
+  <div :class="{ cliente: !isPremium, 'cliente-premium': isPremium }">
+    <!-- <h4>Nome: {{ nome}}</h4>
         <hr>
         <p>{{ descricao}}</p>
         <hr>
         <p>E-mail: {{ email}}</p> -->
-        <!-- ======================= -->
-        <h5>Cliente Objeto</h5>
-        <p>{{cliente.nome}}</p>
-        <p v-if="showIdade == true">Idade {{ cliente.idade }}</p>
-        <p v-else>Sem idade</p>
-        <p v-show="showEmail === true">E-mail {{cliente.email}}</p>
-    </div>
+    <!-- ======================= -->
+    <h5>Cliente Objeto</h5>
+    <p>{{ cliente.nome }}</p>
+    <p v-if="showIdade == true">Idade {{ cliente.idade }}</p>
+    <p v-else>Sem idade</p>
+    <p v-show="showEmail === true">E-mail {{ cliente.email | processarEmail }}</p>
+    <button @click="mudarCor($event)">Mudar cor</button>
+    <button @click="emitirEventoDelete($event)">Deletar</button>
+	<h4>Id especial: {{ idEspecial }}</h4>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-      return {
-        //   numero: '',
-        //   email: 'email@testes.com.br', 
-        //   descricao: 'Lorem ipsum',
-        // isPremium: false,
-      }
+    return {
+      //   numero: '',
+      //   email: 'email@testes.com.br',
+      //   descricao: 'Lorem ipsum',
+      isPremium: false,
+    };
   },
   props: {
-      nome: String,
-      cliente: Object,
-      idade: Number,
-      showIdade: Boolean,
-      showEmail: Boolean,
-      isPremium: Boolean
+    nome: String,
+    cliente: Object,
+    idade: Number,
+    showIdade: Boolean,
+    showEmail: Boolean,
+    //   isPremium: Boolean
+  },
+  methods: {
+    mudarCor: function ($event) {
+      console.log($event);
+      this.isPremium = !this.isPremium;
+    },
+    emitirEventoDelete: function () {
+      console.log("emitir");
+      this.$emit("meDelete", {
+        idCliente: this.cliente.id,
+        curso: "node",
+        component: this,
+      });
+    },
+    testar() {
+      console.log("Testando");
+    },
+  },
+  filters: {
+    processarEmail: function (value) {
+      return value.toUpperCase();
+    },
+  },
+  computed: {
+	idEspecial: function() {
+		return (this.cliente.email + this.cliente.nome + this.cliente.id).toUpperCase();
+	}
   }
-}
+};
 </script>
 
 <style scoped>
-    .cliente {
-        background: rgb(245, 242, 242);
-        padding: 5px;
-        margin: 3px;
-        border: 1px solid rgba(0,0,0,0.3);
-        border-radius: 12px;
-    }
+.cliente {
+  background: rgb(245, 242, 242);
+  padding: 5px;
+  margin: 3px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 12px;
+}
 
-    .cliente-premium {
-        background: rgb(32, 31, 31);
-        color: azure;
-        padding: 5px;
-        margin: 3px;
-        border: 1px solid rgba(0,0,0,0.3);
-        border-radius: 12px;
-    }
+.cliente-premium {
+  background: rgb(32, 31, 31);
+  color: azure;
+  padding: 5px;
+  margin: 3px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 12px;
+}
 </style>
